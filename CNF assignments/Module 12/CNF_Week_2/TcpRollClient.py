f4eae15bcbe69456ae458a06ae0345d9214580e2
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-12-22 12:13:25 +0530

    m12

--- CNF assignments/Module 12/CNF_Week_2/TcpRollClient.py
+++ CNF assignments/Module 12/CNF_Week_2/TcpRollClient.py
@@ -3,24 +3,20 @@ import socket
 def main():
 	host = '127.0.0.1'
 	port = 5000
-
 	s = socket.socket()
-	s.connect((host, port))
+	s.connect((host,port))
 
-	message = input("MARK-ATTENDANCE " + rollnumbers)
-
+	message = input("Enter roll number: ")
 	while message != 'q':
 		s.send(str.encode(message))
 		data = s.recv(1024)
 		data = data.decode()
-		print("Received from server: " + str(data))
-		tokens = str(data).split(" ")
-		if tokens[0] == "":
-			print("")
-			s.close()
-			return
+		if data == "ATTENDANCE SUCCESS":
+			print("Response from server: " + data.decode())
+			break
+		print("Response from server: " + data.decode())
 		message = input("-> ")
 	s.close()
 
 if __name__ == '__main__':
-	main()
+	main() 
